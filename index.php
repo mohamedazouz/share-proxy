4bb6787f1c440f47eaccfa4adb15ea443c3ce25f
Mohamed Azouz <mohamedaliazouz@gmail.com>
2011-03-09 08:57:11 +0000

    add twitter auth

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
 
+?>
 
-<a href="https://www.facebook.com/dialog/oauth?client_id=185034264867265&redirect_uri=http://local.activedd.com/azouz/fb_authenticate.php?code&scope=publish_stream,offline_access" > get your access token</a>
+<a href="http://local.activedd.com/azouz/twitter_authenticate.php" > get your twitter</a>-
+
+<!--<a href="https://www.facebook.com/dialog/oauth?client_id=185034264867265&redirect_uri=http://local.activedd.com/azouz/fb_authenticate.php?code&scope=publish_stream,offline_access" > get your access token</a>-->
