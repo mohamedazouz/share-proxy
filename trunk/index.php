16793967bf384604c29d97d85f2126d3d76e90e2
mohamedaliazouz <mohamedaliazouz@f746304f-f8bf-7716-6818-af080514aa54>
2011-03-09 11:40:55 +0000

    bullding twitter backend to get token

--- trunk/index.php
+++ trunk/index.php
@@ -1,7 +1,20 @@
-<?php
 
-?>
 
-<a href="http://local.activedd.com/azouz/twitter_authenticate.php" > get your twitter</a>-
+<!--<a href="http://local.activedd.com/azouz/twitter_authenticate.php" > get your twitter</a>--->
 
 <!--<a href="https://www.facebook.com/dialog/oauth?client_id=185034264867265&redirect_uri=http://local.activedd.com/azouz/fb_authenticate.php?code&scope=publish_stream,offline_access" > get your access token</a>-->
+
+
+<?php
+include 'test/keys.php';
+include 'test/EpiCurl.php';
+include 'test/EpiOAuth.php';
+include 'test/EpiTwitter.php';
+
+$Twitter = new EpiTwitter($consumerKey, $consumerSecret);
+
+echo '<a href="' . $Twitter->getAuthenticateUrl() . '">
+    sign in
+</a>';
+?>
+
