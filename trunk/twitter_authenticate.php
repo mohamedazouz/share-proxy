4d8ddadbfe15950707417e2b3111a043a4be4fdd
mohamedaliazouz <mohamedaliazouz@f746304f-f8bf-7716-6818-af080514aa54>
2011-03-09 08:57:11 +0000

    add twitter auth

---
+++ trunk/twitter_authenticate.php
@@ -0,0 +1,23 @@
+<?php
+
+$consumer_key = "zxZejhvKQhUqk0c4aYqmiA";
+$consumer_secret = "MmsCp0ZJndjv0kHVx8uXQWgjO95J69nuHurrSxqw8";
+
+echo "eshta";
+
+/*require_once('tw_test/config.php');
+
+
+require_once('tw_test/twitterOAuth/twitterOAuth.php');
+require_once('tw_test/twitterOAuth/OAuth.php');
+
+
+$to = new TwitterOAuth($consumer_key, $consumer_secret);
+$tok = $to->getRequestToken();
+
+$request_link = $to->getAuthorizeURL($tok);
+
+echo $tok['oauth_token'];
+echo "<br> eshts => ".$tok['oauth_token_secret'];*/
+
+?>
