0f7c72028ea15b0f4aa1998b52f5b6d13ab00e96
mohamedaliazouz <mohamedaliazouz@f746304f-f8bf-7716-6818-af080514aa54>
2011-03-09 14:21:03 +0000

    set twitter status with new lib

--- trunk/index.php
+++ trunk/index.php
@@ -1,20 +1,20 @@
-
-
-<!--<a href="http://local.activedd.com/azouz/twitter_authenticate.php" > get your twitter</a>--->
-
-<!--<a href="https://www.facebook.com/dialog/oauth?client_id=185034264867265&redirect_uri=http://local.activedd.com/azouz/fb_authenticate.php?code&scope=publish_stream,offline_access" > get your access token</a>-->
-
-
 <?php
-include 'test/keys.php';
+include 'lib/keys.php';
+include 'lib/EpiCurl.php';
+include 'lib/EpiOAuth.php';
+include 'lib/EpiTwitter.php';
+
+/*include 'test/keys.php';
 include 'test/EpiCurl.php';
 include 'test/EpiOAuth.php';
-include 'test/EpiTwitter.php';
+include 'test/EpiTwitter.php';*/
+
+echo "hi";
 
 $Twitter = new EpiTwitter($consumerKey, $consumerSecret);
 
 echo '<a href="' . $Twitter->getAuthenticateUrl() . '">
-    sign in
+   get your access token twitter
 </a>';
 ?>
-
+<a href="https://www.facebook.com/dialog/oauth?client_id=185034264867265&redirect_uri=http://local.activedd.com/azouz/fb_authenticate.php?code&scope=publish_stream,offline_access" > get your access token from facebook</a>
