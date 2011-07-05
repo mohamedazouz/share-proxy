acc1274b5e89863d5527ec766248b7240f032299
Mohamed Azouz <mohamedaliazouz@gmail.com>
2011-07-05 13:16:20 +0000

    final CRX

--- twitter_authenticate.php
+++ twitter_authenticate.php
@@ -1,5 +1,4 @@
 <?php
-
 include 'headers.php';
 
 
@@ -11,23 +10,25 @@ if (isset($_GET['oauth_token']) || (isset($_COOKIE['oauth_token']) && isset($_CO
     $Twitter->setToken($token->oauth_token, $token->oauth_token_secret);
 
 
-    
+
     $user = $Twitter->get_accountVerify_credentials();
-  
+
 
 
     $json = array();
     $json['oauth_token'] = $token->oauth_token;
-    $json['oauth_token_secret']= $token->oauth_token_secret;
-    $json['screen_name']=$user->screen_name;
-    $json['profile_image_url']= $user->profile_image_url;
-    
+    $json['oauth_token_secret'] = $token->oauth_token_secret;
+    $json['screen_name'] = $user->screen_name;
+    $json['profile_image_url'] = $user->profile_image_url;
+
     session_start();
     $_SESSION['twitter_token'] = json_encode($json); // store session data
     //$status = $Twitter->post('/statuses/update.json', array('status' => 'This a simple test from twitter-async at ' . date('m-d-Y h:i:s')));
 }
 ?>
 
-<script>
-    setTimeout("window.close()", 2*1000);
+<script type="text/javascript">
+    document.write('تم اضافة الحساب بنجاح ');
+    setTimeout('window.close();', 1*1000);
+    setTimeout('window.location="http://twitter.com";', 2*1000);
 </script>
